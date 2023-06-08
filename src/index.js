de422c37070908e5c65284b9222211cafd393d97
Ajiboso-Adeola <adecom112@gmail.com>
2023-06-09 00:58:54 +0100

    Frontend development

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
-// API key and endpoint URL
 let apiKey = "8b3c2188b1fe57f318bdcd6231e30d94";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
 
-// DOM elements
 let searchBox = document.querySelector(".search input");
 let searchButton = document.querySelector(".search button");
 let weather_icon = document.querySelector(".weather-icon");
 
-// Variable to store Celsius value
-let cel;
+let cel; // Celsius value storage
 
-// Function to check weather for a city
 async function checkWeather(city) {
-  // Fetch weather data from the API
-  const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+  try {
+    const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
 
-  // Handle 404 (city not found) response
-  if (response.status === 404) {
-    document.querySelector(".err").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-  } else {
-    // Parse the response data
+    if (!response.ok) {
+      throw new Error("Unable to fetch weather data.");
+    }
+
     const data = await response.json();
 
-    // Update the HTML with weather information
     document.querySelector(".city").innerHTML = data.name;
     const tempCelcius = Math.round(data.main.temp);
     document.querySelector(".temp").innerHTML = tempCelcius + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".pressure").innerHTML = data.main.pressure;
 
-    // Update weather icon based on weather condition
     if (data.weather[0].main === "Clouds") {
       weather_icon.src = "../images/clouds.png";
     } else if (data.weather[0].main === "Clear") {
@@ -44,36 +36,35 @@ async function checkWeather(city) {
       weather_icon.src = "../images/mist.png";
     }
 
-    // Show weather section and hide error message
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".err").style.display = "none";
 
     // Store the Celsius value
     cel = tempCelcius;
+  } catch (error) {
+    document.querySelector(".err").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+    console.error(error);
   }
 }
 
-// Event listener for search button click
 searchButton.addEventListener("click", () => {
   const city = searchBox.value.trim();
   if (city !== "") {
-    // Call checkWeather function with the entered city
     checkWeather(city);
   }
 });
 
-// Event listener for Fahrenheit button click
+// Convert Celsius to Fahrenheit
 document.getElementById("farenheit").addEventListener("click", () => {
-  // Convert Celsius to Fahrenheit and update the HTML
   if (cel !== undefined) {
     let fer = Math.floor(cel * 1.8 + 32);
     document.querySelector(".temp").innerHTML = fer + "°F";
   }
 });
 
-// Event listener for Celsius button click
+// Restore the Celsius value
 document.getElementById("celcius").addEventListener("click", () => {
-  // Restore the Celsius value and update the HTML
   if (cel !== undefined) {
     document.querySelector(".temp").innerHTML = cel + "°C";
   }
